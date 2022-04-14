f6b67a8901f0ca2f3757e1286390f9f2e4cd40de
kyle.macasinag <kylenmac@verizon.net>
2022-04-14 10:53:20 -0400

    Implemented POST request fuctionality

--- products/views.py
+++ products/views.py
@@ -2,12 +2,16 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .serializers import ProductSerializer
 from .models import Product
-
-@api_view(['GET'])
+from rest_framework import status
+@api_view(['GET', 'POST'])
 def product_list(request):
-    products = Product.objects.all()
 
-    serializer = ProductSerializer(products, many=True)
-
-
-    return Response(serializer.data)
+    if request.method == 'GET':
+        products = Product.objects.all()
+        serializer = ProductSerializer(products, many=True)
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        serializer = ProductSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
